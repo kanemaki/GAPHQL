1abeadb2bdb94d4d438ee7bfb470bc02118f65ac
Alexandre Florentino <kanemaki@gmail.com>
2021-08-02 15:40:19 +0100

    feat: create first type using all types of scalar GarphQL.

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const {ApolloServer, gql} = require('apollo-server')
 const typeDefs = gql`
     scalar Date
 
+    type Usuario {
+        id: ID
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
     # Pontos de entrada da minha API!
     type Query {
-        ola: String
-        horaAtual: Date    
+        ola: String!
+        horaAtual: Date!
+        usuarioLogado: Usuario    
     }
 `
 const resolvers = {
@@ -16,6 +26,19 @@ const resolvers = {
 
         horaAtual() {
             return new Date()
+        },
+
+        usuarioLogado() {
+            return {
+                id: 1,
+                nome:  'kanemaki',
+                email: 'kanemaki@kanemaki.com',
+                idade: 41,
+                salario: 1234.56,
+                vip: true
+
+            }
+
         }
 
     }
