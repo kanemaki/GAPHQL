60e42d242c31b166c484e9e0c8650a428ff00458
Alexandre Florentino <kanemaki@gmail.com>
2021-08-02 20:07:26 +0100

    feat: add enum statusUsuario for Usuario.

--- data/db.js
+++ data/db.js
@@ -1,7 +1,7 @@
 const usuarios = [
-    { id: 1, nome: 'Joao Silva', email: 'jsilva@email.com', idade: 29, perfil_id: 1},
-    { id: 2, nome: 'Rafael Junior', email: 'rafajun@email.com', idade: 31, perfil_id: 2},
-    { id: 3, nome: 'Ada Lovelace', email: 'adalove@email.com', idade: 24, perfil_id: 1}
+    { id: 1, nome: 'Joao Silva', email: 'jsilva@email.com', idade: 29, perfil_id: 1, status: 'ATIVO'},
+    { id: 2, nome: 'Rafael Junior', email: 'rafajun@email.com', idade: 31, perfil_id: 2, status: 'INATIVO'},
+    { id: 3, nome: 'Ada Lovelace', email: 'adalove@email.com', idade: 24, perfil_id: 1, status: 'BLOQUEADO'}
 ]
 
 const perfis = [
